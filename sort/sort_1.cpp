8e75b6a662b1fc66d5821269ad2ba4682145fb9a
gilee <weg901127@naver.com>
2022-06-15 09:46:12 +0900

    day01

---
+++ sort/sort_1.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    vector<int> tmp(0);
+    for (int i = 0 ; i < commands.size() ; i++) {
+        tmp.assign(array.begin() + (commands[i][0] - 1), array.begin() + ((commands[i][1])));
+        std::sort(tmp.begin(), tmp.begin() + tmp.size());
+        answer.push_back(tmp[commands[i][2] - 1]);
+    }
+    return answer;
+}
+int main(void) {
+    solution({1,2,3,4,5,6,7}, {{2,5,3},{4,4,1},{1,7,3}});
+    return 0;
+}
